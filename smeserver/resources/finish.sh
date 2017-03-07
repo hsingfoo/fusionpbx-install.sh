6bfa835b29d8e805fd4e3841640b5becdfb52f3b
hsingfoo <hsingfoo@gmail.com>
2017-03-07 14:38:37 +0100

    WIP

--- smeserver/resources/finish.sh
+++ smeserver/resources/finish.sh
@@ -76,7 +76,7 @@ sed -i /etc/freeswitch/autoload_configs/xml_cdr.conf.xml -e s:"{v_user}:$xml_cdr
 sed -i /etc/freeswitch/autoload_configs/xml_cdr.conf.xml -e s:"{v_pass}:$xml_cdr_password:"
 
 #app defaults
-cd /opt/fusionpbx && /opt/fusionpbx/core/upgrade/upgrade_domains.php
+cd /opt/fusionpbx && php /opt/fusionpbx/core/upgrade/upgrade_domains.php
 
 # Setting fusionpbx db keys
 config set fusionpbx configuration DomainName $domain_name DBName fusionpbx DBUser $user_name DBPassword $user_password XMLCDRUser $xml_cdr_username XMLCDRPassword $xml_cdr_password
