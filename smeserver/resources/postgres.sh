b648fbe070a91f0c0d2130f9f0069bf5824d7e12
hsingfoo <hsingfoo@gmail.com>
2017-03-11 09:52:16 +0100

    WIP

--- smeserver/resources/postgres.sh
+++ smeserver/resources/postgres.sh
@@ -41,8 +41,8 @@ usermod -a -G www postgres
 # Move to /tmp to prevent a red herring error when running sudo with psql
 cwd=$(pwd)
 cd /tmp
-sudo -u postgres psql -d fusionpbx -c "DROP SCHEMA public cascade;";
-sudo -u postgres psql -d fusionpbx -c "CREATE SCHEMA public;";
+#sudo -u postgres psql -d fusionpbx -c "DROP SCHEMA public cascade;";
+#sudo -u postgres psql -d fusionpbx -c "CREATE SCHEMA public;";
 sudo -u postgres psql -c "CREATE DATABASE fusionpbx";
 sudo -u postgres psql -c "CREATE DATABASE freeswitch";
 sudo -u postgres psql -c "CREATE ROLE fusionpbx WITH SUPERUSER LOGIN PASSWORD '$password';"
