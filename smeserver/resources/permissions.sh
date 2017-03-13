33b895545cf7c0894087ba2d1acfb9660afc48c8
hsingfoo <hsingfoo@gmail.com>
2017-03-13 11:39:49 +0100

    WIP

--- smeserver/resources/permissions.sh
+++ smeserver/resources/permissions.sh
@@ -7,11 +7,11 @@ cd "$(dirname "$0")"
 # Set ownership and permission
 
 chown -R www:www /etc/freeswitch /var/lib/freeswitch /var/log/freeswitch /usr/share/freeswitch
-find /etc/freeswitch -type d -exec chmod 770 {} \;
+find /etc/freeswitch -type d -exec chmod 775 {} \;
 find /var/lib/freeswitch -type d -exec chmod 770 {} \;
-find /var/log/freeswitch -type d -exec chmod 770 {} \;
-find /usr/share/freeswitch -type d -exec chmod 770 {} \;
+find /var/log/freeswitch -type d -exec chmod 775 {} \;
+find /usr/share/freeswitch -type d -exec chmod 775 {} \;
 find /etc/freeswitch -type f -exec chmod 664 {} \;
 find /var/lib/freeswitch -type f -exec chmod 664 {} \;
 find /var/log/freeswitch -type f -exec chmod 664 {} \;
-find /usr/share/freeswitch -type f -exec chmod 770 {} \;
+find /usr/share/freeswitch -type f -exec chmod 775 {} \;
