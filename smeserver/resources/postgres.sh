557a61ac4b58197c4c41296d710d131a34bbc287
hsingfoo <hsingfoo@gmail.com>
2017-03-13 14:46:07 +0100

    WIP

--- smeserver/resources/postgres.sh
+++ smeserver/resources/postgres.sh
@@ -10,8 +10,9 @@ echo ""
 verbose "Installing and configuring PostgreSQL 9.4"
 
 #generate a random password
-password=$(dd if=/dev/urandom bs=1 count=20 2>/dev/null | base64)
-#password=supersecret
+#password=$(dd if=/dev/urandom bs=1 count=20 2>/dev/null | base64)
+password=supersecret
+
 #Install and configure PostgreSQL 9.4
 yum -y -q install postgresql94-server postgresql94-contrib postgresql94 --enablerepo=postgresql94
 ln -s /etc/rc.d/init.d/e-smith-service /etc/rc7.d/S64postgresql-9.4
@@ -20,15 +21,13 @@ config setprop postgresql-9.4 status enabled
 config setprop postgresql-9.4 TCPPort 5432
 config setprop postgresql-9.4 UDPPort 5432
 config setprop postgresql-9.4 access private
-config setprop postgresql-9.4 FusionDBPassword $password
-config setprop postgresql-9.4 FreeswitchDBPassword $password
 signal-event remoteaccess-update
 
 # Initialize PostgreSQL database
 /etc/rc.d/init.d/postgresql-9.4 initdb
 
 # Adjust /var/lib/pgsql/9.4/data/pg_hba.conf to MD5 local users
-sed -i 's/\(local  *all  *all    *\)peer/\1trust/' /var/lib/pgsql/9.4/data/pg_hba.conf
+#sed -i 's/\(local  *all  *all    *\)peer/\1trust/' /var/lib/pgsql/9.4/data/pg_hba.conf
 sed -i 's/\(host  *all  *all  *127.0.0.1\/32  *\)ident/\1trust/' /var/lib/pgsql/9.4/data/pg_hba.conf
 sed -i 's/\(host  *all  *all  *::1\/128  *\)ident/\1trust/' /var/lib/pgsql/9.4/data/pg_hba.conf
 
