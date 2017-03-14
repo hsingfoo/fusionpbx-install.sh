999a427c6be9a0e7271c0e09d6f07e8d71c9b58f
hsingfoo <hsingfoo@gmail.com>
2017-03-14 19:00:19 +0100

    WIP

--- smeserver/resources/smeserver.sh
+++ smeserver/resources/smeserver.sh
@@ -24,6 +24,7 @@ config set memcached service status enabled
 #Install php56-php-fpm (from remi repo for SCL environment)
 echo ""
 verbose "Installing and configuring php56-php-fpm"
+yum -y -q install php56-php-fpm --enablerepo=remi
 ln -s /etc/rc.d/init.d/e-smith-service /etc/rc7.d/S98php56-php-fpm
 config set php56-php-fpm service TCPPort 9000 status enabled
 sed -ie "s|listen = 127.0.0.1:9000|listen = /opt/remi/php56/root/var/run/php-fpm/php-fpm.sock|g" /opt/remi/php56/root/etc/php-fpm.d/www.conf
