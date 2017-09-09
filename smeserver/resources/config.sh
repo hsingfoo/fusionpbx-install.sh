8b6ebd60e437427a95ee158dcff7ec04fd3cf886
hsingfoo <hsingfoo@gmail.com>
2017-09-09 13:06:20 +0200

    WIP

--- smeserver/resources/config.sh
+++ smeserver/resources/config.sh
@@ -13,7 +13,7 @@ database_backup=false           					# true or false
 web_server_name=apache          					# nginx, apache
 
 # PostgreSQL details
-database_version=9.4								# 9.4, 9.6
+database_version=9.6								# 9.4, 9.6
 database_host=127.0.0.1
 database_port=5432
 database_username=fusionpbx
