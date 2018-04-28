8cdfb2184b5c23ced8cd134b2c4c2dde36fc94ef
hsingfoo <hsingfoo@gmail.com>
2018-04-28 12:26:43 +0200

    WIP

--- smeserver/resources/branding.sh
+++ smeserver/resources/branding.sh
@@ -17,17 +17,17 @@ mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/com
 mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
 mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
 
-cp resources/branding/20ProductName /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
-cp resources/branding/25Copyright /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
-cp resources/branding/25ProductSMEServer /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp smeserver/resources/branding/20ProductName /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp smeserver/resources/branding/25Copyright /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp smeserver/resources/branding/25ProductSMEServer /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
 
-cp resources/branding/20Title /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
-cp resources/branding/30FrameSetup01 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
-cp resources/branding/30FrameSetup02 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp smeserver/resources/branding/20Title /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp smeserver/resources/branding/30FrameSetup01 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp smeserver/resources/branding/30FrameSetup02 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
 
-cp resources/branding/10Head /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
-cp resources/branding/40LogoRow /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
-cp resources/branding/template-begin /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp smeserver/resources/branding/10Head /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp smeserver/resources/branding/40LogoRow /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp smeserver/resources/branding/template-begin /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
 
 # Set root permissions and ownership on custom templates
 chown -R root:root /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/*
