b7bffa9399f0dc54ffb6754bea6d120aef7852a6
hsingfoo <hsingfoo@gmail.com>
2018-04-28 21:21:11 +0200

    WIP

--- smeserver/resources/redis.sh
+++ smeserver/resources/redis.sh
@@ -15,10 +15,12 @@ yum $AUTO --enablerepo=epel install redis
 config set redis service status enabled
 ln -s /etc/rc.d/init.d/e-smith-service /etc/rc7.d/S80redis
 usermod -a -G redis www
-sed -i 's|# unixsocket.*|unixsocket /var/run/redis/redis.sock|' /etc/redis.conf
-sed -i 's|# unixsocketperm.*|unixsocketperm 777|' /etc/redis.conf
-mkdir -p /var/run/redis
-chown -R redis:redis /var/run/redis/redis.sock
-/etc/rc.d/init.d/redis start
 
-# Note: phpxx-php-pecl-redis (for nextcloud) already installed by php-scl procedure
+# Replace default values is /etc/rdeis.conf
+sed -i "s|# unixsocket /tmp/redis.sock|unixsocket /var/run/redis/redis.sock|" /etc/redis.conf
+sed -i 's|# unixsocketperm 700|unixsocketperm 777|' /etc/redis.conf
+
+# Create /var/run/redis and set permissions
+mkdir -p /var/run/redis
+chown -R redis:redis /var/run/redis
+/etc/rc.d/init.d/redis start
