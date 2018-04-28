54b3ae3c4ee9f287f8d15d2df79c13d06c68ab1a
hsingfoo <hsingfoo@gmail.com>
2018-04-28 11:24:55 +0200

    WIP

--- smeserver/resources/branding.sh
+++ smeserver/resources/branding.sh
@@ -14,14 +14,14 @@ mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/com
 mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
 mkdir -p /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
 
-cp ./branding/20ProductName /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
-cp ./branding/25Copyright /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
-cp ./branding/25ProductSMEServer /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp resources/branding/20ProductName /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp resources/branding/25Copyright /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
+cp resources/branding/25ProductSMEServer /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/foot.tmpl
 
-cp ./branding/20Title /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
-cp ./branding/30FrameSetup01 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
-cp ./branding/30FrameSetup02 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp resources/branding/20Title /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp resources/branding/30FrameSetup01 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
+cp resources/branding/30FrameSetup02 /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/common/head.tmpl
 
-cp ./branding/10Head /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
-cp ./branding/40LogoRow /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
-cp ./branding/template-begin /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp resources/branding/10Head /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp resources/branding/40LogoRow /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
+cp resources/branding/template-begin /etc/e-smith/templates-custom/etc/e-smith/templates/etc/e-smith/web/panels/manager/html/header.htm
