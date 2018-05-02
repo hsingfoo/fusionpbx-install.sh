89b3b09c6d229ade24ba3de0f929fb2c6c7b90ec
hsingfoo <hsingfoo@gmail.com>
2018-05-02 20:40:13 +0200

    WIP

--- smeserver/resources/nextcloud.sh
+++ smeserver/resources/nextcloud.sh
@@ -43,7 +43,7 @@ cd $cloud_path
 git submodule update --init -q
 cd "$(dirname "$0")"
 
-# Auto configure Nextcloud
+# Auto configure file for Nextcloud
 cat <<HERE1 > $cloud_path/config/autoconfig.php
 <?php
 $AUTOCONFIG = array(
@@ -91,8 +91,8 @@ cat <<HERE3 > $cloud_path/config/rewrite.config.php
 HERE3
 
 # Adjust .htaccess to remove index.php in the URL for cosmetic reasons
-cd $cloud_path
-sudo -u www scl enable php$php_version 'php occ maintenance:mode --on'
-sudo -u www scl enable php$php_version 'php occ maintenance:update:htaccess'
-sudo -u www scl enable php$php_version 'php occ maintenance:mode --off'
-cd "$(dirname "$0")"
+#cd $cloud_path
+#sudo -u www scl enable php$php_version 'php occ maintenance:mode --on'
+#sudo -u www scl enable php$php_version 'php occ maintenance:update:htaccess'
+#sudo -u www scl enable php$php_version 'php occ maintenance:mode --off'
+#cd "$(dirname "$0")"
